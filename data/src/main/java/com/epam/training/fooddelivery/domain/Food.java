f64b88204eeb1c5f99bde3eda76143a8338bbee0
Jakub Wójcik <jakub.wojcik.c@gmail.com>
2022-08-15 17:47:43 +0200

    solution-2 - add setters to Food class

--- data/src/main/java/com/epam/training/fooddelivery/domain/Food.java
+++ data/src/main/java/com/epam/training/fooddelivery/domain/Food.java
@@ -79,4 +79,28 @@ public class Food {
     public Category getCategory() {
         return category;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCalorie(BigDecimal calorie) {
+        this.calorie = calorie;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
