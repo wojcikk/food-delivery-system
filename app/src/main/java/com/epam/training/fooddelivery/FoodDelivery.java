96cb8081c0c8d858add0771c69ca42701812fb84
Jakub Wójcik <jakub.wojcik.c@gmail.com>
2022-08-15 18:23:30 +0200

    solution-6 - authenticate method refactor

--- app/src/main/java/com/epam/training/fooddelivery/FoodDelivery.java
+++ app/src/main/java/com/epam/training/fooddelivery/FoodDelivery.java
@@ -1,17 +1,18 @@
 package com.epam.training.fooddelivery;
 
+import com.epam.training.fooddelivery.domain.Customer;
+import com.epam.training.fooddelivery.domain.Food;
+import com.epam.training.fooddelivery.domain.Order;
+import com.epam.training.fooddelivery.domain.User;
 import com.epam.training.fooddelivery.repository.CustomerRepository;
 import com.epam.training.fooddelivery.repository.FoodRepository;
 import com.epam.training.fooddelivery.repository.OrderItemRepository;
 import com.epam.training.fooddelivery.repository.OrderRepository;
-import com.epam.training.fooddelivery.domain.*;
 import com.epam.training.fooddelivery.service.*;
 import com.epam.training.fooddelivery.view.CLIView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 
 @Component
 public class FoodDelivery implements CommandLineRunner {
@@ -48,8 +49,6 @@ public class FoodDelivery implements CommandLineRunner {
         User user = view.readCredentials();
 
         Customer customer = customerService.authenticate(user);
-        customer.setCart(new Cart());
-        customer.setOrders(new ArrayList<>());
 
         view.printWelcomeMessage(customer);
 
--- app/src/main/java/com/epam/training/fooddelivery/service/DefaultCustomerService.java
+++ app/src/main/java/com/epam/training/fooddelivery/service/DefaultCustomerService.java
@@ -1,10 +1,13 @@
 package com.epam.training.fooddelivery.service;
 
+import com.epam.training.fooddelivery.domain.Cart;
 import com.epam.training.fooddelivery.repository.CustomerRepository;
 import com.epam.training.fooddelivery.domain.Customer;
 import com.epam.training.fooddelivery.domain.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 public class DefaultCustomerService implements CustomerService {
@@ -20,6 +23,8 @@ public class DefaultCustomerService implements CustomerService {
         for(Customer customer: customerRepository.findAll()) {
             if(customer.getEmail().equals(user.getEmail()) &&
                     customer.getPassword().equals(user.getPassword())) {
+                customer.setCart(new Cart());
+                customer.setOrders(new ArrayList<>());
                 return customer;
             }
         }
